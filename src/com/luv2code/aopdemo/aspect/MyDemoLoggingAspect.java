bea8facf0ba5e5d12ebf634043cfd7e3fd43eebb
Litsakis <litsakis@outlook.com>
2022-10-13 21:15:23 +0300

    Read method args with JoinPoints

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,10 +1,13 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
 
 @Aspect
 @Component
@@ -17,9 +20,36 @@ public class MyDemoLoggingAspect {
 	 
 	
 	@Before("com.luv2code.aopdemo.aspect.AopExpessions.finalpointcut()")
-	public void beforeAddAcoountAdvice() {
+	public void beforeAddAcoountAdvice(JoinPoint theJoinPoint) {
 		
 		System.out.println("\n===================>>>>>>>  Executing @beforeAddAcoountAdvice()");
+		
+		
+		
+		//display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: "+methodSig);
+		
+		//display the method arguments
+		//get args
+		
+		Object[] args= theJoinPoint.getArgs();
+
+		//loop thru arg
+				
+				for (Object tempArg : args) {
+					System.out.println(tempArg);
+					
+					
+					if (tempArg instanceof AccountDAO) {
+						//downcast and print Account specific stuff
+						AccountDAO theAccount = (AccountDAO) tempArg;
+						
+						
+					}
+					
+				}
 	}
 	
 
